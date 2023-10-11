72f237067b64cc1101cbcec2831ee8794b993b6b
AliaksandrMakaranka <aliaksandrmakaranka@gmail.com>
2023-10-11 22:42:04 +0200

    how this work

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,10 +2,10 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        int c = (char) 'c';
-        int h = (char) 'h';
-        int a = (char) 'a';
-        int r = (char) 'r';
+        int c = (int) 'c';
+        int h = (int) 'h';
+        int a = (int) 'a';
+        int r = (int) 'r';
         System.out.println(c);
         System.out.println(h);
         System.out.println(a);
