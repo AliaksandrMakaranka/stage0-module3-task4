f608af9ccaf2a374b99200486514d33fb2c26ad6
AliaksandrMakaranka <aliaksandrmakaranka@gmail.com>
2023-10-11 12:19:39 +0200

    intRepesentation and SettingType ready

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c = (char) 'c';
+        int h = (char) 'h';
+        int a = (char) 'a';
+        int r = (char) 'r';
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(r);
     }
 }
