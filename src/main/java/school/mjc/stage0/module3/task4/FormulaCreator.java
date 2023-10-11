6ef8ff8ea279a83b28b35c1037f3e0155492dc18
AliaksandrMakaranka <aliaksandrmakaranka@gmail.com>
2023-10-11 12:27:00 +0200

    f

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 2;
+        int b = 2;
+
+        int f = 9 * (a * a) - 5 * b + 2 + a - 7;
+        int s = (a + b - 4 * a * b) / 2;
+        int result = f * s;
+        System.out.println(result);
     }
 }
